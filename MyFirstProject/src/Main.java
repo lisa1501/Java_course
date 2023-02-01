66a0e62b9dad4001667c7f8a75cc38e74ce8ecb5
Lisa S <halisasaipulla@gmail.com>
2023-02-01 13:48:45 -0800

    Practice on Java Variables

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        // int x; //declaration
+        
+        // x = 123; // assignment
+
+        // int x = 123; // initialization
+
+        long x = 123456789987456L;
+
+        System.out.println(x);
+        System.out.println("My number is: "+x);
+
+        // float y = 3.14f;
+
+        double y = 3.14;
+        System.out.println(y);
+
+        boolean z = true;
+        System.out.println(z);
+
+        char symbol = '@';
+        System.out.println(symbol);
+
+        // String name = 'Lsia'; // must be double quoutes
+        String name = "Lisa";
+        System.out.println(name);
+
+        
         
     }
 }
