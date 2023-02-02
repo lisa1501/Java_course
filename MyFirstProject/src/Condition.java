88f50c215ae90e7c21323b6b01a5c7b97a5c6e50
Lisa S <halisasaipulla@gmail.com>
2023-02-01 16:01:03 -0800

    if statements

---
+++ MyFirstProject/src/Condition.java
@@ -0,0 +1,17 @@
+public class Condition {
+    public void condition() {
+        //if statement  = performs a block of code if it's condition evaluates to be true
+        int age = 72;
+
+        if(age>=72){
+            System.out.println("Ok Boomer!");
+        }
+        else if(age>=18) {
+            System.out.println("You are an adult");
+        }
+        else{
+            System.out.println("You are not an adult");
+        }
+    
+    }
+}
