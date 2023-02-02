4c78b3c7a2f16b652bfff40ec54f0925fe21d63f
halisa <halisasaipulaa@gmail.com>
2023-02-02 14:24:45 -0800

    For each loop

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,11 +1,12 @@
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        TwoDArrayList twoDArrayList = new TwoDArrayList();
-        twoDArrayList.twoDArrayList();
+        Foreachloop foreachloop = new Foreachloop();
+        foreachloop.foreachloop();
+
+        // TwoDArrayList twoDArrayList = new TwoDArrayList();
+        // twoDArrayList.twoDArrayList();
 
         // Arraylist arraylist = new Arraylist();
         // arraylist.arraylist();
