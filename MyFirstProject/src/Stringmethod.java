3a620023d3669dc324f0b02c8c85a085cf14631b
halisa <halisasaipulaa@gmail.com>
2023-02-02 11:50:47 -0800

    String methods

---
+++ MyFirstProject/src/Stringmethod.java
@@ -0,0 +1,37 @@
+public class Stringmethod {
+    public void stringmethod(){
+        // string =  a reference date type that can store one 
+        //           or more characters
+        //           reference data types have access useful methods 
+    
+        String name = "Halisa";
+        
+        // boolean result = name.equals("halisa");
+
+        // boolean result = name.equalsIgnoreCase("halisa");
+
+        // int result = name.length();
+        
+        // char result = name.charAt(2);
+
+        // int result = name.indexOf("l");
+
+        // boolean result = name.isEmpty();
+
+        // String result = name.toUpperCase();
+
+        // String result = name.toLowerCase();
+
+        // String name = "   Halisa   ";
+        // String result = name.trim();
+
+        String result = name.replace("s", "d");
+
+        System.out.println(result);
+
+        
+
+    
+    }
+    
+}
