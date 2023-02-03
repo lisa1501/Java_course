05d31902233d69881c1ff8f583b6d75106fbd81b
Lisa S <halisasaipulla@gmail.com>
2023-02-03 10:30:50 -0800

    OOP --- constructors

---
+++ OOP/src/Human.java
@@ -0,0 +1,21 @@
+public class Human {
+
+    String name;
+    int age;
+    double height;
+
+    Human(String name, int age, double height){
+
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void eat(){
+        System.out.println(this.name+" is eating");
+    }
+
+    void drink(){
+        System.out.println(this.name+" is drinking water");
+    }
+}
--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,19 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        Car myCar1 = new Car();
-        Car myCar2 = new Car();
 
-        System.out.println(myCar1.color);
-        System.out.println(myCar1.model);
-        System.out.println();
-        System.out.println(myCar2.color);
-        System.out.println(myCar2.model);
+        // construntor = special method that is called when an object
+        //                  is instantianted (created)
 
+        Human human1 = new Human("Lisa",21,165);
+        Human human2 = new Human("Heidi",20,155);
         
-        myCar1.drive();
-        myCar1.brake();
-        System.out.println();
-        myCar2.drive();
-        myCar2.brake();
+        human1.eat();
+        human2.drink();
+
+        // System.out.println(human1.name);
+        // System.out.println(human2.name);
+
+
+
+        // Car myCar1 = new Car();
+        // Car myCar2 = new Car();
+
+        // System.out.println(myCar1.color);
+        // System.out.println(myCar1.model);
+        // System.out.println();
+        // System.out.println(myCar2.color);
+        // System.out.println(myCar2.model);
+
+
+        // myCar1.drive();
+        // myCar1.brake();
+        // System.out.println();
+        // myCar2.drive();
+        // myCar2.brake();
     }
 }
