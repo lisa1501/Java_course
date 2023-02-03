0115796c50579fda774c0eb32498f43fe7aa3d4d
Lisa S <halisasaipulla@gmail.com>
2023-02-03 11:31:08 -0800

    OOP--- overloaded constructors

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,9 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
+        // overloaded constructors = multiple constructores within a class wiht the same name , 
+        //                           but have different parameters
+        //                           name + parameters = signature
+
+        Pizza veganpizza = new Pizza("thicc crust","tomato");
+        System.out.println("Here are the ingredients of your pizza ");
+        System.out.println(veganpizza.bread);
+        System.out.println(veganpizza.sauce);
+
+        Pizza pizza = new Pizza("thicc crust","tomato","mozzerella","pepperoni");
+        System.out.println("Here are the ingredients of your pizza ");
+        System.out.println(pizza.bread);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.topping);
+        
+        
         // local = diclared inside a method visible only to that method
         // global = declared outside a method, but within a class visible to all parts of a class
-        DiceRoller diceRoller = new DiceRoller();
+        
+        // DiceRoller diceRoller = new DiceRoller();
+        
         // construntor = special method that is called when an object
         //                  is instantianted (created)
 
