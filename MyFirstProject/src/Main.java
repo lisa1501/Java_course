5dcc576bf69b4015c6f4223004a50a640c0f20aa
halisa <halisasaipulaa@gmail.com>
2023-02-02 16:09:17 -0800

    Methods

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Foreachloop foreachloop = new Foreachloop();
-        foreachloop.foreachloop();
+        // Foreachloop foreachloop = new Foreachloop();
+        // foreachloop.foreachloop();
 
         // TwoDArrayList twoDArrayList = new TwoDArrayList();
         // twoDArrayList.twoDArrayList();
---
+++ MyFirstProject/src/Methods.java
@@ -0,0 +1,34 @@
+public class Methods {
+
+    public static void main(String[] args) {
+        // method = a block of code that is executed whenever it is called upon
+    
+    
+    //     String name ="Lisa";
+    //     int age = 21;
+        
+    //     hello(name,age);
+    // }
+    // static void hello(String name, int age){
+    //     System.out.println("Hello "+name);
+    //     System.out.println("You are "+age);
+    // }
+
+    // static void hello(String title){
+    //     System.out.println("Hello "+title);
+    // }
+
+
+        int x = 3;
+        int y = 4;
+        
+        System.out.println(add(x,y));
+    }
+    static int add(int x, int y){
+
+        return  x+y;
+        
+    }
+
+}
+
