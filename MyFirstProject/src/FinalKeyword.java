5f2a8e7629cd01e0f7eefd78cba460dff2d1ac8e
Lisa S <halisasaipulla@gmail.com>
2023-02-02 18:22:49 -0800

    Final keyword

---
+++ MyFirstProject/src/FinalKeyword.java
@@ -0,0 +1,11 @@
+public class FinalKeyword {
+    public void finalKeyword(){
+
+        // final double pi =3.14159;
+        // pi = 4;
+        // System.out.println(pi);
+        //  The final local variable pi cannot be assigned. It must be blank and not using a compound assignment
+        final double PI = 3.14159;
+        System.out.println(PI);
+    }
+}
