ad92918a0a1acdefb3a688d9f58684b4eadf7889
halisa <halisasaipulaa@gmail.com>
2023-02-03 09:54:01 -0800

    objects(OOP)

---
+++ OOP/src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year = 2000;
+    String color = "blue";
+    double price = 5000.00;
+
+    void drive(){
+        System.out.println("You drive the car");
+    }
+    
+    void brake(){
+        System.out.println("You step on the brakes");
+    }
+}
