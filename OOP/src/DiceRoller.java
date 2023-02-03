930772c1322700ebbbfb4cd957cb0177485f26f6
halisa <halisasaipulaa@gmail.com>
2023-02-03 11:09:57 -0800

    Variable Scope--Global variable

--- OOP/src/DiceRoller.java
+++ OOP/src/DiceRoller.java
@@ -1,15 +1,17 @@
 import java.util.Random;
 
-import java.util.Random;
 public class DiceRoller {
     
+    Random random;
+    int number;
+
     DiceRoller(){
-        Random random = new Random();
-        int number = 0;
-        roll(random,number);
+        random = new Random();
+        
+        roll();
     }
 
-    void roll(Random random, int number){
+    void roll(){
         number = random.nextInt(6)+1;
         System.out.println(number);
     }
