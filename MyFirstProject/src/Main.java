db34bdc4988345991f4b48740530f78e6f7e35af
halisa <halisasaipulaa@gmail.com>
2023-02-02 18:22:49 -0800

    Final keyword

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -2,9 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        FinalKeyword finalKeyword = new FinalKeyword();
+        finalKeyword.finalKeyword();
 
-        Printf printf = new Printf();
-        printf.printf();
+        // Printf printf = new Printf();
+        // printf.printf();
         // Overload overload = new Overload();
         // overload.add(1,2);
 
