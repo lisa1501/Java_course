4516e3f12c927bf9082a2d4b9f6c675248ca76ac
Lisa S <halisasaipulla@gmail.com>
2023-02-03 17:07:45 -0800

    static keyword

---
+++ OOP/src/Friend.java
@@ -0,0 +1,15 @@
+public class Friend {
+
+    String name;
+    static int numberofFriends;
+
+    Friend(String name){
+        this.name = name;
+        numberofFriends++;
+    }
+
+    static void displayFriends(){
+        System.out.println("You have "+numberofFriends+" friends.");
+    }
+    
+}
