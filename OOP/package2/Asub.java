b5881be4dc44d96ca5acab660d2e49df50daa9be
Lisa S <halisasaipulla@gmail.com>
2023-02-04 11:43:29 -0800

    access modifiers--protected

--- OOP/package2/Asub.java
+++ OOP/package2/Asub.java
@@ -6,8 +6,10 @@ public class Asub extends A {
     
     public static void main(String[] args) {
         
-        C c = new C();
+        Asub asub = new Asub();
 
-        System.out.println(c.defaultMessage);
+        System.out.println(asub.protectedMessage);
+
     }
+
 }
