139dde8e66432ae6810274a45c887276cc451212
halisa <halisasaipulaa@gmail.com>
2023-02-03 16:35:38 -0800

    array of objects

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,12 +1,33 @@
 public class Main {
     public static void main(String[] args) {
+
+        // int[] numbers = new int[3];
+        // char[] characters = new char[4];
+        // String[] strings = new String[5];
+
+        // Food[] refrigerator = new Food[3];
+
+        Food food1 = new Food("pizza");
+        Food food2 = new Food("hamburger");
+        Food food3 = new Food("hotdog");
+
+        
+
+        // refrigerator[0]=food1;
+        // refrigerator[1]=food2;
+        // refrigerator[2]=food3;
+
+        Food[] refrigerator = {food1,food2,food3};
+        System.out.println(refrigerator[0].name);
+        System.out.println(refrigerator[1].name);
+        System.out.println(refrigerator[2].name);
         // toString() = special method that all objects inherit,
         //              that returns a string that "textually represents" an object
         //              can be used both implicitly and explocitly
 
-        Car car = new Car();
+        // Car car = new Car();
         
-        System.out.println(car.toString());
+        // System.out.println(car.toString());
         
 
         // overloaded constructors = multiple constructores within a class wiht the same name , 
