6747a062f3813922efca010874106d17ec937e2b
halisa <halisasaipulaa@gmail.com>
2023-02-04 13:32:07 -0800

    polymorphism

---
+++ OOP/package5/Bicycle.java
@@ -0,0 +1,8 @@
+package OOP.package5;
+
+public class Bicycle extends Vehicle{
+    @Override
+    public void go(){
+        System.out.println("*The bicycle begins moving");
+    }
+}
---
+++ OOP/package5/Boat.java
@@ -0,0 +1,8 @@
+package OOP.package5;
+
+public class Boat extends Vehicle {
+    @Override
+    public void go(){
+        System.out.println("*The boat begins moving");
+    }
+}
