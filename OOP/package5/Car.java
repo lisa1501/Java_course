9ce6f4b8c1a23d371045adebde2d0e723adc58d0
Lisa S <halisasaipulla@gmail.com>
2023-02-04 13:32:07 -0800

    polymorphism

---
+++ OOP/package5/Car.java
@@ -0,0 +1,8 @@
+package OOP.package5;
+
+public class Car extends Vehicle {
+    @Override
+    public void go(){
+        System.out.println("*The car begins moving");
+    }
+}
---
+++ OOP/package5/Vehicle.java
@@ -0,0 +1,8 @@
+package OOP.package5;
+
+public class Vehicle {
+
+    public void go() {
+    }
+    
+}
